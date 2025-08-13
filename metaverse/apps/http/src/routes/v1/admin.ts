7d448080a78fa66cabdc0c97842ced5f4c1638b8
Deekshith <deekshithv.cy22@rvce.edu.in>
2025-08-14 01:59:04 +0530

    Thumbnail addded| Admin routes

--- metaverse/apps/http/src/routes/v1/admin.ts
+++ metaverse/apps/http/src/routes/v1/admin.ts
@@ -1,20 +1,86 @@
 import { Router } from "express";
+import { adminMiddleware } from "../../middlewares/admin";
+import { AddanElementSchema, CreatAvatarSchema, CreateElementSchema, CreateMapSchema, UpdateElementSchema } from "../../types/types";
+import db from "@repo/db/db";
+export const adminRouter = Router();
+adminRouter.use(adminMiddleware)
 
-export const adminRouter=Router();
+adminRouter.post("/element", async (req, res) => {
+    const parsedData = CreateElementSchema.safeParse(req.body)
+    if (!parsedData.success) {
+        res.status(400).json({message: "Validation failed"})
+        return
+    }
 
-adminRouter.get("/element",(req,res)=>{
+    const element = await db.element.create({
+        data: {
+            width: parsedData.data.width,
+            height: parsedData.data.height,
+            static: parsedData.data.static,
+            imageUrl: parsedData.data.imageUrl,
+        }
+    })
 
+    res.json({
+        id: element.id
+    })
 })
 
-adminRouter.put("/element/:eleId",(req,res)=>{
-
+adminRouter.put("/element/:elementId", (req, res) => {
+    const parsedData = UpdateElementSchema.safeParse(req.body)
+    if (!parsedData.success) {
+        res.status(400).json({message: "Validation failed"})
+        return
+    }
+    db.element.update({
+        where: {
+            id: req.params.elementId
+        },
+        data: {
+            imageUrl: parsedData.data.imageUrl
+        }
+    })
+    res.json({message: "Element updated"})
 })
 
-adminRouter.put("/avatar",(req,res)=>{
-    
+adminRouter.post("/avatar", async (req, res) => {
+    const parsedData = CreatAvatarSchema.safeParse(req.body)
+    if (!parsedData.success) {
+        res.status(400).json({message: "Validation failed"})
+        return
+    }
+    const avatar = await db.avatar.create({
+        data: {
+            name: parsedData.data.name,
+            imageUrl: parsedData.data.imageUrl
+        }
+    })
+    res.json({avatarId: avatar.id})
 })
 
-adminRouter.put("/map",(req,res)=>{
-    
-})
+adminRouter.post("/map", async (req, res) => {
+    const parsedData = CreateMapSchema.safeParse(req.body)
+    if (!parsedData.success) {
+        res.status(400).json({message: "Validation failed"})
+        return
+    }
+    const map = await db.map.create({
+        data: {
+            name: parsedData.data.name,
+            width: parseInt(parsedData.data.dimensions.split("x")[0]),
+            height: parseInt(parsedData.data.dimensions.split("x")[1]),
+            thumbnail: parsedData.data.thumbnail,
+            mapElements: {
+                create: parsedData.data.defaultElements.map((e:any) => ({
+                    elementId: e.elementId,
+                    x: e.x,
+                    y: e.y
+                }))
+            }
+        }
+    })
 
+    res.json({
+        id: map.id
+    })
+})
